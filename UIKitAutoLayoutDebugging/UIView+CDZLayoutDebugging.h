c9efd7021cec317658507a6feca664f6251e4b12
Chris Dzombak <chris@chrisdzombak.net>
2015-03-11 16:48:44 -0400

    Implement UIView identifier totally in the header :/

--- UIKitAutoLayoutDebugging/UIView+CDZLayoutDebugging.h
+++ UIKitAutoLayoutDebugging/UIView+CDZLayoutDebugging.h
@@ -1,22 +1,25 @@
 @import Foundation;
 @import UIKit.UIView;
 
-static NSString *CDZLayoutDebuggingIdentifierSelectorString() {
+/**
+ Set the given view's private layout debugging identifier.
+ This makes various Auto Layout constraint debug outputs easier to read.
+
+ This entire function is a no-op when the macro `#if DEBUG` evaluates to true.
+ 
+ It's [unfortunately] implemented as a C function in the header so `#DEBUG`
+ can be evaluated in the context of your project at each build time. This
+ is necessary to avoid the private selector's name being included in your
+ app for App Store submissions.
+ */
+static inline void CDZSetLayoutDebuggingIdentifier(UIView *view, NSString *identifier) {
 #if DEBUG
-    return @"_setLayoutDebuggingIdentifier:";
-#else
-    return nil;
+    SEL debugSelector = NSSelectorFromString(@"_setLayoutDebuggingIdentifier:");
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    if ([view respondsToSelector:debugSelector]) {
+        [view performSelector:debugSelector withObject:identifier];
+    }
+#pragma clang diagnostic pop
 #endif
 }
-
-@interface UIView (CDZLayoutDebugging)
-
-/**
- Set this view's private layout debugging identifier.
- This makes various Auto Layout constraint debug output easier to read.
-
- This entire method is a no-op when the macro `#if DEBUG` evaluates to true.
- */
-- (void)cdz_setLayoutDebugIdentifier:(NSString *)identifier;
-
-@end
