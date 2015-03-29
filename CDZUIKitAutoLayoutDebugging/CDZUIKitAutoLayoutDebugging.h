0fde18fe098928e3c5f6969508fafdccbaaae0b9
Chris Dzombak <chris@chrisdzombak.net>
2015-03-29 17:17:01 -0400

    Add CDZ prefix to everything

---
+++ CDZUIKitAutoLayoutDebugging/CDZUIKitAutoLayoutDebugging.h
@@ -0,0 +1,10 @@
+@import Foundation;
+
+//! Project version number for CDZUIKitAutoLayoutDebugging.
+FOUNDATION_EXPORT double CDZUIKitAutoLayoutDebuggingVersionNumber;
+
+//! Project version string for CDZUIKitAutoLayoutDebugging.
+FOUNDATION_EXPORT const unsigned char CDZUIKitAutoLayoutDebuggingVersionString[];
+
+#import <CDZUIKitAutoLayoutDebugging/UIView+CDZLayoutDebugging.h>
+#import <CDZUIKitAutoLayoutDebugging/NSLayoutConstraint+CDZLayoutDebugging.h>
